0473b8abc243288743561c594e742fbea1955421
Sanskriti Singh <146508510+Sanskriti3104@users.noreply.github.com>
2024-12-14 23:03:50 +0530

    Time: 1 ms (93.98%), Space: 44.5 MB (58.38%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,31 +1,33 @@
 class Solution {
-    public void swap(int i,int j,int nums[]){
-        int t = nums[i];
-        nums[i] = nums[j];
-        nums[j] = t;
-    }
+   public void swap(int nums[] , int i,int j){
+    int temp = nums[i];
+    nums[i]= nums[j];
+    nums[j]= temp;
+   }
 
-    public void findPermutation(int nums[],int index,List<List<Integer>> result){
-        if(index == nums.length){
-            List<Integer> ds = new ArrayList<>();
-            for(int i=0; i<nums.length; i++){
-                ds.add(nums[i]);
+   public void findPermutation(int nums[],int idx,List<List<Integer>> result){
+        if(idx == nums.length){
+            // If we've used all elements, add the current permutation to the result
+            List<Integer> currentPermutation = new ArrayList<>();
+            for (int num : nums) {
+                currentPermutation.add(num);
             }
-            result.add(new ArrayList<>(ds));
+            result.add(currentPermutation);
             return;
         }
-        for(int i= index;i<nums.length;i++){
-            swap(i,index,nums);
-            findPermutation(nums,index + 1,result);
-            swap(i,index,nums);
+
+        for(int i = idx; i<nums.length; i++){
+            // Swap to fix the current element
+            swap(nums, idx, i);
+            // Recurse for the next index
+            findPermutation(nums, idx + 1, result);
+            // Backtrack: revert the swap
+            swap(nums, idx, i);
         }
-    }
-
+   }
     public List<List<Integer>> permute(int[] nums) {
-
         List<List<Integer>> result = new ArrayList<>();
         findPermutation(nums,0,result);
         return result;
-        
     }
 }
