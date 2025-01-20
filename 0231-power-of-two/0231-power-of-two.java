2aa907abe301cdc26c69e606cb80915575c17cfd
Sanskriti Singh <146508510+Sanskriti3104@users.noreply.github.com>
2025-01-20 22:01:14 +0530

    Time: 0 ms (100%), Space: 40.9 MB (47.02%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,46 +1,11 @@
 class Solution {
-    public String DecimalToBinary(int num){
-        if (num == 0) { 
-            return "0";
-        }
-
-        StringBuilder str = new StringBuilder();
-        while (num > 0) {
-            if(num % 2 == 1){
-                str.append(1);
-            }else{
-                str.append(0);
-            }
-
-            num = num / 2;
-        }
-
-        return str.reverse().toString();
-    }
-
     public boolean isPowerOfTwo(int n) {
-        if(n < 0){
+        
+        if(n>0 && (n & (n - 1)) == 0){
+            return true;
+        }else{
             return false;
         }
-        String toBinary = DecimalToBinary(n);
-        Boolean isPowerTwo = true;
-        for(int i =0; i< toBinary.length(); i++){
-            if(i == 0){
-                if( toBinary.charAt(i) == '1'){
-                    continue;
-                }else{
-                    return false;
-                }
-            }else{
-                if( toBinary.charAt(i) == '1'){
-                    isPowerTwo = false;
-                    return isPowerTwo;
-                }else{
-                    continue;
-                }
-            }
-        }
 
-        return isPowerTwo;
     }
 }
