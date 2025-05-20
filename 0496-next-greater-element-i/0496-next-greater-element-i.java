081b56a109accb32ab139c6157d213bb968e7cb2
Sanskriti Singh <146508510+Sanskriti3104@users.noreply.github.com>
2025-05-20 22:46:34 +0530

    Time: 3 ms (90.62%), Space: 44.2 MB (38.26%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,36 +1,26 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        
-        int[] nge = new int[nums1.length];
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        Stack<Integer> s = new Stack<>();
+        Stack<Integer> st = new Stack<>();
+        HashMap<Integer,Integer> hm = new HashMap();
+        int[] result = new int[nums1.length];
 
-        for(int i=nums2.length-1; i>=0; i--){
-            if(s.isEmpty()){
-                hashMap.put(nums2[i], -1);
-                s.push(nums2[i]);
-            }else{
-                if(nums2[i] > s.peek()){
-                    while(!s.isEmpty() && nums2[i] >= s.peek()){
-                        s.pop();
-                    }
-                    if(s.isEmpty()){
-                        hashMap.put(nums2[i], -1);
-                    }else{
-                        hashMap.put(nums2[i], s.peek());
-                    }
-                    s.push(nums2[i]);
-                }else{
-                    hashMap.put(nums2[i], s.peek());
-                    s.push(nums2[i]);
-                }
+        for(int i = nums2.length - 1; i >= 0; i--){
+            while(!st.empty() && st.peek() <= nums2[i] ){
+                st.pop();
             }
+
+            if(st.isEmpty()){
+                hm.put(nums2[i],-1);
+            }else{
+                hm.put(nums2[i],st.peek());
+            }
+            st.push(nums2[i]);
         }
 
         for(int i=0; i<nums1.length; i++){
-            nge[i] = hashMap.get(nums1[i]);
+            result[i] = hm.get(nums1[i]);
         }
 
-        return nge;
+        return result;
     }
 }
