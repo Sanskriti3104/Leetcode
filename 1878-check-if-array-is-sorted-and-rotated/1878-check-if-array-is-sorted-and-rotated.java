a8efeaa27740cf19723dcd7008a3d99724a9c6d6
Sanskriti Singh <146508510+Sanskriti3104@users.noreply.github.com>
2025-11-21 14:43:48 +0530

    Time: 0 ms (100%), Space: 42.5 MB (27.97%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -1,33 +1,26 @@
 class Solution {
     public boolean check(int[] nums) {
-        int index = 0;
         boolean flag = true;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i + 1] < nums[i]) {
-                index = i + 1;
+        int pos = 0;
+
+        for(int i =0; i<nums.length-1; i++){
+            if(nums[i] > nums[i+1]) {   
+                pos = i+1;
                 break;
             }
         }
 
-        if (index == 0) {
-            for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] > nums[i + 1]) {
-                    flag = false;
-                    return flag;
-                }
-            }
-        } else {
-            for (int i = index; i < nums.length - 1; i++) {
-                if (nums[i] > nums[i + 1]) {
-                    flag = false;
-                    return flag;
-                }
-            }
-            if(nums[nums.length-1] > nums[0]){
+        if( pos == 0 )  return flag;
+
+        for(int i= pos; i< nums.length-1; i++){
+            if(nums[i] > nums[i+1]){
                 flag = false;
-                return flag;
+                break;
             }
         }
+
+        if(nums[0] < nums[nums.length -1 ]) flag = false;
+
         return flag;
     }
 }
