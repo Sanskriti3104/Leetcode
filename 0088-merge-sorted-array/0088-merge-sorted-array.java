2b350d3f5c9d9d3cf667c4e355e35a07f82050c3
Sanskriti Singh <146508510+Sanskriti3104@users.noreply.github.com>
2026-01-18 02:36:23 +0530

    Time: 0 ms (100%), Space: 44.1 MB (9.81%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,37 +1,26 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int left = 0;
-        int right = 0;
-        int[] arr = new int[m+n];
-        int idx =0;
+        int a = m-1; // last index of initialized nums1
+        int b = m+n-1;  // last index of nums1 total
+        int c = n-1;    // last index of nums2
 
-        while(left < m && right < n){
-            if (nums1[left] < nums2[right]){
-                arr[idx] = nums1[left];
-                idx++;
-                left++;
+        while(a >= 0 && c >= 0){
+            if(nums1[a]<nums2[c]){
+                nums1[b] = nums2[c];
+                c--;
             }else{
-                arr[idx] = nums2[right];
-                idx++;
-                right++;
+                nums1[b] = nums1[a];
+                a--;
             }
+            b--;
         }
 
-        while(left < m){
-            arr[idx] = nums1[left];
-            idx++;
-            left++;
-        } 
-
-        while(right < n){
-            arr[idx] = nums2[right];
-            idx++;
-            right++;
+        // If nums2 still has elements, copy them
+        while (c >= 0) {
+            nums1[b] = nums2[c];
+            b--;
+            c--;
         }
-
-        for(int i=0; i<m+n; i++ ){
-            nums1[i] = arr[i];
-        }
-
+        return;
     }
 }
