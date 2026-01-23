5f98c4a3928de6bbc457d98a6ba00071895d15f1
Sanskriti Singh <146508510+Sanskriti3104@users.noreply.github.com>
2026-01-23 23:19:45 +0530

    Time: 0 ms (100%), Space: 45 MB (19.07%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -8,10 +8,10 @@ class Solution {
 
             if(nums[mid] == target){
                 return mid;
-            }else if(target < nums[mid]){
-                right = mid-1;
-            }else{
+            }else if(target > nums[mid]){
                 left = mid+1;
+            }else{
+                right = mid-1;
             }
         }
 
