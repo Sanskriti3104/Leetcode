6c4220be3431224bf9cf4e6a77763f532b938d16
Sanskriti Singh <146508510+Sanskriti3104@users.noreply.github.com>
2026-06-29 00:18:48 +0530

    Time: 0 ms (100%), Space: 46.7 MB (61.96%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,14 +1,22 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int i=0;
+        //Two pointers
+        int left =0;
+        int right =1;
 
-       for(int j=1; j<nums.length; j++){
-            if(nums[i] != nums[j]){
-                i++;
-                nums[i] = nums[j];
+        while(right < nums.length){
+            //Skip duplicate elements
+            if(nums[left] == nums[right])   right++;
+            //Place the next unique element at the write position
+            else{
+                left++;
+                nums[left] = nums[right];
+                right++;
             }
         }
 
-        return i+1;
+        return left+1;
     }
 }
+//Time Complexity - O(N)
+//Space Complexity - O(1)
