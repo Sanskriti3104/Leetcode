912038d7270d5fbb480fd3f1d314a9f703d9bfde
Sanskriti Singh <146508510+Sanskriti3104@users.noreply.github.com>
2026-06-29 20:59:50 +0530

    Time: 0 ms (100%), Space: 47.2 MB (81.49%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,12 +1,19 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int totalSum = ( nums.length * ( nums.length + 1))/2;
-        int sum = 0;
+        int n = nums.length;
 
-        for(int i=0; i<nums.length; i++){
-            sum += nums[i];
+        //Calculate the expected sum of numbers from 0 to n
+        int totalSum = (n*(n+1))/2;
+        int givenSum = 0;
+
+        //Calculate the sum of the given array elements
+        for(int i=0; i<n; i++){
+            givenSum += nums[i];
         }
 
-        return totalSum - sum;
+        //The difference gives the missing number
+        return totalSum - givenSum;
     }
 }
+//Time Complexity - O(N)
+//Space Complexity - O(1)
