570e0a416410088c0958550c68ca65d18234b8d9
Sanskriti Singh <146508510+Sanskriti3104@users.noreply.github.com>
2026-07-24 07:03:39 +0530

    Time: 7 ms (97.94%), Space: 65.2 MB (58.21%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,48 +1,39 @@
 class Solution {
-    static boolean isPalindrome(String temp){
-        boolean palindrome = true;
-        if(temp.length() == 1)  return palindrome;
-
-        int l =0;
-        int r =temp.length()-1;
-
-        while(l<r){
-            if(temp.charAt(l) == temp.charAt(r)){
-                l++;
-                r--;
-            }else{
-                palindrome = false;
-                break;
-            }
-        }
-
-        return palindrome;
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        List<String> current = new ArrayList<>();
+        palindromePartition(result,current,0,s);
+        return result;
     }
-    static void findPartition(int idx,String s,List<List<String>> result,List<String> substring){
+    private boolean palindrome (String s, int start, int end){
+        while(start < end){
+            if(s.charAt(start) != s.charAt(end))    return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+    private void palindromePartition(List<List<String>> result,List<String> current,int idx,String s){
         if(idx == s.length()){
-            result.add(new ArrayList<>(substring));
+            result.add(new ArrayList<>(current));
             return;
         }
 
-        for(int i=idx; i<s.length(); i++){
-            if(isPalindrome(s.substring(idx, i+1))){
-                substring.add(s.substring(idx, i+1));
-                findPartition(i+1,s,result,substring);
-                substring.remove(substring.size() -1);
+        for(int i = idx; i<s.length(); i++){
+            if(palindrome(s,idx,i)){
+                current.add(s.substring(idx,i+1));
+                palindromePartition(result,current,i+1,s);
+                current.remove(current.size() -1);
             }
         }
     }
-    public List<List<String>> partition(String s) {
-        List<List<String>> result = new ArrayList<>();
-        List<String> substring = new ArrayList<>();
-        
-        if(s.length() == 1){
-            substring.add(s);
-            result.add(new ArrayList<>(substring));
-            return result;
-        }
-
-        findPartition(0,s,result,substring);
-        return result;
-    }
 }
+//Time Complexity
+//There are approximately O(2^n) possible partitions.
+//For each partition, checking palindromes can take O(n)
+//Therefore = O(n × 2^n)
+
+//Space Complexity
+//Recursion stack O(n)
+//Current partition O(n)
+//Output excluded.
