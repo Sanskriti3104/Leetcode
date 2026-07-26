babea1eaadd172937539deb113ea660880b65815
Sanskriti Singh <146508510+Sanskriti3104@users.noreply.github.com>
2026-07-27 02:37:26 +0530

    Time: 1 ms (96.63%), Space: 42.4 MB (87.64%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
+        if(n<0 || n==0)return false;
+        if(n == 1)return true;
 
-        if(n>0 && (n & (n - 1)) == 0){
-            return true;
-        }else{
-            return false;
-        }
-        
+        if((n & (n-1)) == 0)return true;
+
+        return false;
     }
 }
